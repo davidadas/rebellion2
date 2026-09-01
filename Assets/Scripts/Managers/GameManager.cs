93eac47e2fb22f4c1cf0f560dd206cf753e10443
David Adams <djadams2007@gmail.com>
2026-09-01 11:12:05 -0400

    Remove duplicate recovery initialization

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -455,7 +455,6 @@ public sealed class GameManager
         _duelSystem = new DuelSystem(_game, _randomProvider);
         _movementSystem = new MovementSystem(_game, _fogOfWarSystem, _fleetSystem, _blockadeSystem);
         _headquartersSystem = new HeadquartersSystem(_game, _movementSystem);
-        _recoverySystem = new RecoverySystem(_game);
         _manufacturingSystem = new ManufacturingSystem(_game, _fleetSystem, _movementSystem);
         _namingSystem = new NamingSystem(_game);
         _recoverySystem = new RecoverySystem(_game);
